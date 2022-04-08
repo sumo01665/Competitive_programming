694a8ad3c305ac3ec23fa6b6c40d4ac3ea359130
Trist T <ttknguyen2015@gmail.com>
2022-04-07 22:59:43 -0400

    4/7/22 Merge Sort

--- Leetcode/Array/88_Merge_Sorted_array.cpp
+++ Leetcode/Array/88_Merge_Sorted_array.cpp
@@ -9,7 +9,17 @@ typedef unsigned long long ULL;
 
 //Function 
 void merge(vector<int>& nums1, int m, vector<int>& nums2, int n) {
-        
+   int i = m - 1, j = n -1, k = m+n-1;
+   while (i >= 0 && j >=0){
+        if (nums1[i] > nums2[j]){
+           nums1[k] = nums1[i];
+        }
+        else{
+           nums1[k] = nums1[j];     
+        }
+       
+   }
+    
 
 }
 //Template
