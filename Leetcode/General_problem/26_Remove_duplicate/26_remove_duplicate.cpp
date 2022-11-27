9abfffd5efd0955f564b807567bcf545f1c5103d
Trist T <ttknguyen2015@gmail.com>
2022-11-27 06:08:09 -0500

    Work on LC75 Problem - Day 1 - Task 3

--- Leetcode/General_problem/26_Remove_duplicate/26_remove_duplicate.cpp
+++ Leetcode/General_problem/26_Remove_duplicate/26_remove_duplicate.cpp
@@ -1,1 +1,29 @@
-#
+#include <bits/stdc++.h>
+
+// Two arrays solution 
+class Solution {
+public:
+    int removeDuplicates(vector<int>& nums) {
+        int remove; 
+        for (int i = 1; i < nums.size(); ++i){ 
+            if (nums[i] == nums[i - 1]){ 
+                nums.erase(nums.begin() + i); 
+
+
+            }
+
+
+        }
+
+
+    
+    }    
+   void  print_array(vector<int> nums){
+        for (int i = 0; i < nums.size(); ++i){
+            cout << nums[i]
+
+            if (i = nums.size() - 1) cout << "."; 
+            else cou
+        }
+
+};
