2ccf4bdfada3d88647d0db32f8eb6d5b1cf545ff
Trist T <t35truon@uwaterloo.ca>
2023-06-22 16:31:18 -0400

    Add 714_best_time_to_buy

---
+++ Leetcode/Leetcode_solution/714.best-time-to-buy-and-sell-stock-with-transaction-fee.cpp
@@ -0,0 +1,18 @@
+/*
+ * @lc app=leetcode id=714 lang=cpp
+ *
+ * [714] Best Time to Buy and Sell Stock with Transaction Fee
+ */
+
+// @lc code=start
+class Solution {
+public:
+    int maxProfit(vector<int>& prices, int fee) {
+        
+
+        return fee;
+
+    }
+};
+// @lc code=end
+
